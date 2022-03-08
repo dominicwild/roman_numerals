2d887e9a257a49fd43ca9c786edaf6c39d50d50f
dwild8 <dwild8@dxc.com>
2022-03-08 11:28:25 +0000

    String replace refactor

--- src/main/java/com/roman_numerals/RomanNumerals.java
+++ src/main/java/com/roman_numerals/RomanNumerals.java
@@ -3,13 +3,36 @@ package com.roman_numerals;
 public class RomanNumerals {
 
     public static String convert(int toConvert) {
-        StringBuilder romanNumeral = new StringBuilder();
+        StringBuilder romanNumeralBuilder = new StringBuilder();
 
         for (int i = 0; i < toConvert; i++) {
-            romanNumeral.append("I");
+            romanNumeralBuilder.append("I");
         }
 
-        return romanNumeral.toString().replace("IIIII", "V").replace("VV", "X").replace("XXXXX", "L");
+        String romanNumeralString = romanNumeralBuilder.toString();
+        for (RomanNumeral romanNumeral : RomanNumeral.values()) {
+            romanNumeralString = romanNumeral.replace(romanNumeralString);
+        }
+
+        return romanNumeralString;
+    }
+
+    enum RomanNumeral {
+        V("IIIII", "V"),
+        X("VV", "X"),
+        L("XXXXX", "L");
+
+        private final String romanNumeral;
+        private final String replacement;
+
+        RomanNumeral(String romanNumeral, String replacement) {
+            this.romanNumeral = romanNumeral;
+            this.replacement = replacement;
+        }
+
+        public String replace(String romanNumeral) {
+            return romanNumeral.replace(this.romanNumeral, this.replacement);
+        }
     }
 
 }
